10f6d2dd51acab0cc22117ff8e574586ed9ce4d9
Mark Gelfius <mgelfius@purdue.edu>
2017-06-26 20:58:05 -0400

    Added age and color text field. Text should change to color entered in color field

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ function changeHeading(ev){
     ev.preventDefault();
     const f = ev.target
     const name = f.personName.value;
+    const age = f.personAge.value
+    const textColor = f.color.value
     const heading = document.querySelector("h1")
-    heading.textContent = name;
+    heading.innerHTML = name + "'s Stats"
+    const divName = document.querySelector("#divName")
+    divName.innerHTML = "Name: " + name
+    const divAge = document.querySelector("#divAge")
+    divAge.innerHTML = "Age: " + age
+    document.getElementById("stats").style.color = textColor
 }
 
 const personForm = document.querySelector("#person-form")
