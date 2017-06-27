c7540c52997cb78398d3cc622fd6187bdc477228
Mark Gelfius <mgelfius@purdue.edu>
2017-06-27 15:27:02 -0400

    Add foundation CSS

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 function changeHeading(ev){
-    ev.preventDefault();
+    ev.preventDefault()
     const f = ev.target
-    const name = f.personName.value;
+    const name = f.personName.value
     const age = f.personAge.value
     const textColor = f.color.value
     const gender = f.personGender.value
     const feet = f.heightFeet.value
     const inches = f.heightInches.value
     const bgcolor = f.bgcolor.value
-    const heading = document.querySelector("h1")
-    const divName = document.querySelector("#divName")
-    const divAge = document.querySelector("#divAge")
-    const divGender = document.querySelector("#divGender")
-    const divHeight = document.querySelector("#divHeight")
+    const heading = document.querySelector('h1')
+    const divName = document.querySelector('#divName')
+    const divAge = document.querySelector('#divAge')
+    const divGender = document.querySelector('#divGender')
+    const divHeight = document.querySelector('#divHeight')
     
 
-    heading.innerHTML = name + "'s Stats"
-    divName.innerHTML = "Name: " + name
-    divAge.innerHTML = "Age: " + age
-    divGender.textContent = "Gender: " + gender
-    divHeight.textContent = "Height: " + feet + "' " + inches + '"'
+    heading.innerHTML = name + '\'s Stats'
+    divName.innerHTML = 'Name: ' + name
+    divAge.innerHTML = 'Age: ' + age
+    divGender.textContent = 'Gender: ' + gender
+    divHeight.textContent = 'Height: ' + feet + '\' ' + inches + '"'
 
-    document.getElementById("stats").style.color = textColor
-    document.getElementById("stats").style.background = bgcolor
-    document.getElementById("statsHead").style.color = textColor
-    document.getElementById("statsHead").style.background = bgcolor
+    document.getElementById('stats').style.color = textColor
+    document.getElementById('stats').style.background = bgcolor
+    document.getElementById('statsHead').style.color = textColor
+    document.getElementById('statsHead').style.background = bgcolor
 }
 
-const personForm = document.querySelector("#person-form")
+const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeHeading)
