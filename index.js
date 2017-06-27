272cd9774fd3ae2f2edcaa3a72ed0fecdf8640ca
Mark Gelfius <mgelfius@purdue.edu>
2017-06-27 09:47:30 -0400

    Added gender selection

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ function changeHeading(ev){
     const name = f.personName.value;
     const age = f.personAge.value
     const textColor = f.color.value
+    const gender = f.personGender.value
     const heading = document.querySelector("h1")
     heading.innerHTML = name + "'s Stats"
     const divName = document.querySelector("#divName")
     divName.innerHTML = "Name: " + name
     const divAge = document.querySelector("#divAge")
     divAge.innerHTML = "Age: " + age
+    const divGender = document.querySelector("#divGender")
+    divGender.textContent = "Gender: " + gender
     document.getElementById("stats").style.color = textColor
 }
 
